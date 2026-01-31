71d2737af0391cbcf9833a5eb0dfbc99f2c810f8
陈 . <chen.@chendebijibendiannao.local>
2026-01-31 17:18:26 +0800

    Added initialization check in main

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -20,18 +20,22 @@ public class Main {
                 Repository.init();
                 break;
             case "add":
+                checkInit();
                 validateNumArgs(args, 2);
                 Repository.add(args[1]);
                 break;
             case "commit":
+                checkInit();
                 validateNumArgs(args, 2);
                 Repository.commit(args[1]);
                 break;
             case "rm":
+                checkInit();
                 validateNumArgs(args, 2);
                 Repository.rm(args[1]);
                 break;
             case "log":
+                checkInit();
                 validateNumArgs(args, 1);
                 Repository.log();
 
@@ -52,6 +56,16 @@ public class Main {
     private static void validateNumArgs(String[] args, int n) {
         if (args.length != n) {
             System.out.println("Incorrect operands.");
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Checks if this directory is correctly initialized.
+     */
+    private static void checkInit() {
+        if (!Repository.GITLET_DIR.exists()) {
+            System.out.println("Not in an initialized Gitlet directory.");
             System.exit(0);
         }
     }
